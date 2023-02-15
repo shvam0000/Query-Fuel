b39f3f770132f5a5ca8e4853d6aaef5fbaaa9ae5
shvam0000 <shvam0000@gmail.com>
2023-02-15 20:51:52 +0530

    ✅ add icons

--- src/utils/icons/index.tsx
+++ src/utils/icons/index.tsx
@@ -2,3 +2,6 @@ export { default as Innovation1 } from './innovation1';
 export { default as Innovation2 } from './innovation2';
 export { default as Innovation3 } from './innovation3';
 export { default as Logo } from './logo';
+export { default as Call } from './call';
+export { default as Location } from './location';
+export { default as Contact } from './contact';
